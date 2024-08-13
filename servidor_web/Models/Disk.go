ba4465eaaa188860542895f50fddf9570fa0e8ba
Alejandro-Zam <josea.zambranoj@uqvirtual.edu.co>
2024-08-13 16:51:22 -0500

    0.0.6 Refactor: Disk_Handler, Host_handler,Dashboard_handler. Add: Models: Disk, DashboardData, Utilities: HostQueries, registerElements

---
+++ servidor_web/Models/Disk.go
@@ -0,0 +1,10 @@
+package Models
+
+type Disk struct {
+	Name              string `json:"name"`
+	Ruta_Ubicacion    string `json:"ruta_ubicacion"`
+	Sistema_Operativo string `json:"sistema_operativo"`
+	Distrubucion_SO   string `json:"distrubucion_so"`
+	arquitectura      string `json:"arquitectura"`
+	Host_id           int    `json:"host_id"`
+}
