e3256ab6932f5f6178e1e5d9e5ccfe3583b677ec
Alejandro-Zam <josea.zambranoj@uqvirtual.edu.co>
2024-08-13 14:14:16 -0500

    0.0.5 refactor: funtions host_handler

--- servidor_web/handlers/Host_handler.go
+++ servidor_web/handlers/Host_handler.go
@@ -7,10 +7,11 @@ import (
 	"log"
 
 	"fmt"
+	"net/http"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/goccy/go-json"
-	"net/http"
 )
 
 func CreateHostPage(c *gin.Context) {
