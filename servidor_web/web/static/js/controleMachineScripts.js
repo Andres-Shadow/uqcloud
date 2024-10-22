5fab99b320aed8e06aa760c97c35a45cbdae19e2
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2024-10-21 23:35:10 -0500

    refactor: code style for vm tables changed

--- servidor_web/web/static/js/controleMachineScripts.js
+++ servidor_web/web/static/js/controleMachineScripts.js
@@ -1,12 +1,5 @@
 var ventanaConfiguracionAbierta = false;
 
-function abrirVentanaEmergente() {
-    ventanaConfiguracionAbierta = true;
-
-    var ventanaEmergente = document.getElementById("ventanaEmergente");
-    ventanaEmergente.style.display = "block";
-}
-
 function abrirVentanaEmergenteEliminacion(nombre) {
     ventanaConfiguracionAbierta = true;
 
@@ -63,13 +56,6 @@ function abrirVentanaEmergenteInformacion(nombre, sistemaOperativo, distribucion
     }
 }
 
-function cerrarVentanaEmergente() {
-    ventanaConfiguracionAbierta = false;
-
-    var ventanaEmergente = document.getElementById("ventanaEmergente");
-    ventanaEmergente.style.display = "none";
-}
-
 function cerrarVentanaEmergenteConfiguracion() {
     ventanaConfiguracionAbierta = false;
 
@@ -97,119 +83,60 @@ function actualizarTabla() {
         return;
     }
     $.ajax({
-        url: "/api/machines", // Reemplaza con la URL correcta
+        url: "/api/machines",
         method: "GET",
         dataType: "json",
         success: function (data) {
-            // Limpia la tabla actual
             $("#machine-table tbody").empty();
-            // Itera a través de los datos y agrega filas a la tabla
+
             data.forEach(function (machine) {
-                // console.log("machineestado: ", machine.vm_state);
-                // RECORDAR: el JSON que llega con los machine, viene con el modelo Models.VirtualMachine, como este modelo
-                //           usa 'TAGS' de tipo JSON para codificar y decodificar, para acceder a esos valores se debe usar 
-                //           su tag respectivo. Ej: 
-                //              * machine.Name         | NO SIRVE 
-                //              * machine.vm_name      | SIRVE  (porque en el modelo está asi: `json:"vm_name"`)
+                let backgroundColor;
                 switch (machine.vm_state) {
                     case "Apagado":
-                        // console.log("LLegó aqui-----------------1");
-                        backgroundColor = "#e06666ff"; // Rojo
-                        $("#machine-table tbody").append(
-                            `<tr style="background-color: ${backgroundColor}">
-                                <td>${machine.vm_name}</td>
-                                <td>${machine.vm_ip === "" ? "No asignada" : machine.vm_ip}</td>
-                                <td>${machine.vm_so_distro}</td>
-                                <td>${machine.vm_state}</td>
-                                
-                                <td class="button-column">
-                                    <button onclick="changeStateMachine('${machine.vm_name}', 'startMachine')" class="btn btn-link" style="padding: 0; margin: 0;">
-                                        <img style="width: 35px;" src="/web/static/img/icons/power.png" alt="Botón 1">
-                                    </button>
-                                    <button type="button" class="btn btn-link" style="padding: 0; margin: 0;" onclick="abrirVentanaEmergenteInformacion('${machine.vm_name}','${machine.vm_so}','${machine.vm_so_distro}', '${machine.vm_ip}','${machine.vm_ram}','${machine.vm_cpu}', '${machine.vm_state}', '${machine.vm_hostname}')">
-                                        <img style="width: 35px;" src="/web/static/img/icons/info.png" alt="Botón 4">
-                                    </button>
-                                    <button type="button" class="btn btn-link" style="padding: 0; margin: 0;" onclick="abrirVentanaEmergenteEliminacion('${machine.vm_name}')">
-                                        <img style="width: 30px;" src="/web/static/img/icons/delete.png" alt="Botón 3">
-                                    </button>
-                                </td>
-                                </td>
-                            </tr>`
-                        );
+                        backgroundColor = "#e06666ff";
                         break;
                     case "Encendido":
-                        // console.log("LLegó aqui-----------------2");
-                        backgroundColor = "#93c47dff"; // Verde
-                        $("#machine-table tbody").append(
-                            `<tr style="background-color: ${backgroundColor}">
-                                <td>${machine.vm_name}</td>
-                                <td style="position: relative;">
-                                    ${machine.vm_ip === "" ? "No asignada" : machine.vm_ip}
-                                    
-                                    <!-- Muestra el botón solo si la IP está asignada -->
-                                    ${machine.vm_ip !== "" ? `
-
-                                    <a href="http://${machine.vm_ip}:4200" target="_blank">
-                                        <i class="fa-solid fa-up-right-from-square"></i>
-                                    </a>                           
-                                    ` : ''}
-                                </td>
-
-                                <td>${machine.vm_so_distro}</td>
-                                <td>${machine.vm_state}</td>
-                                
-                                <td class="button-column">
-                                    <button onclick="changeStateMachine('${machine.vm_name}', 'stopMachine')" class="btn btn-link" style="padding: 0; margin: 0;">
-                                        <img style="width: 35px;" src="/web/static/img/icons/power.png" alt="Botón 1">
-                                    </button>
-                                    <button type="button" class="btn btn-link" style="padding: 0; margin: 0;" onclick="abrirVentanaEmergenteInformacion('${machine.vm_name}','${machine.vm_so}','${machine.vm_so_distro}', '${machine.vm_ip}','${machine.vm_ram}','${machine.vm_cpu}', '${machine.vm_state}', '${machine.vm_hostname}', '${machine.vm_hostname}')">
-                                        <img style="width: 35px;" src="/web/static/img/icons/info.png" alt="Botón 3">
-                                    </button>                                    
-                                    <button type="submit" class="btn btn-link" style="padding: 0; margin: 0;" disabled>
-                                        <img style="width: 30px;" src="/web/static/img/icons/delete.png" alt="Botón 4">
-                                    </button>                                    
-                                </td>
-                            </tr>`
-                        );
+                        backgroundColor = "#93c47dff";
                         break;
                     case "Procesando":
-                        // console.log("LLegó aqui-----------------3");
-                        backgroundColor = "#83DEE3"; // Azul
-                        $("#machine-table tbody").append(
-                            `<tr style="background-color: ${backgroundColor}">
-                                <td>${machine.vm_name}</td>
-                                <td>${machine.vm_ip === "" ? "No asignada" : machine.vm_ip}</td>
-                                <td>${machine.vm_so_distro}</td>
-                                <td>${machine.vm_state}</td>
-                            
-                                <td class="button-column">
-                                    <button onclick="changeStateMachine('${machine.vm_name}', 'startMachine')" class="btn btn-link" style="padding: 0; margin: 0;" disabled>
-                                        <img style="width: 35px;" src="/web/static/img/icons/power.png" alt="Botón 1">
-                                    </button>
-                                    <button type="button" class="btn btn-link" style="padding: 0; margin: 0;" onclick="abrirVentanaEmergenteInformacion('${machine.vm_name}','${machine.vm_so}','${machine.vm_so_distro}', '${machine.vm_ip}','${machine.vm_ram}','${machine.vm_cpu}', '${machine.vm_state}', '${machine.vm_hostname}')">
-                                        <img style="width: 35px;" src="/web/static/img/icons/info.png" alt="Botón 4">
-                                    </button>                                
-                                    <button class="btn btn-link" style="padding: 0; margin: 0;" disabled>
-                                        <img style="width: 30px;" src="/web/static/img/icons/delete.png" alt="Botón 3">
-                                    </button>
-                                </td>
-                            </tr>`
-                        );
+                        backgroundColor = "#83DEE3";
                         break;
                     default:
-                        // console.log("LLegó aqui-----------------4");
-                        backgroundColor = ""; // Puedes proporcionar un valor predeterminado si es necesario
+                        backgroundColor = "";
                 }
 
+                const ipDisplay = machine.vm_ip ? machine.vm_state === 'Encendido' ? `${machine.vm_ip} <a href="http://${machine.vm_ip}:4200" target="_blank"><i class="fa-solid fa-up-right-from-square ms-2"></i></a>` : `${machine.vm_ip}` : "No asignada";
+
+                const actionButtons = `
+                    <div class="">
+                        <button class="btn btn-link p-0 me-1" onclick="changeStateMachine('${machine.vm_name}', '${machine.vm_state === 'Apagado' ? 'startMachine' : 'stopMachine'}')">
+                            <i class="fa ${machine.vm_state === 'Apagado' ? 'fa-power-off' : 'fa-stop-circle'} icon-large" title="${machine.vm_state === 'Apagado' ? 'Start' : 'Stop'}"></i>
+                        </button>
+                        <button class="btn btn-link p-0 me-1" onclick="abrirVentanaEmergenteInformacion('${machine.vm_name}', '${machine.vm_so}', '${machine.vm_so_distro}', '${machine.vm_ip}', '${machine.vm_ram}', '${machine.vm_cpu}', '${machine.vm_state}', '${machine.vm_hostname}')">
+                            <i class="fa fa-info-circle icon-large" title="Info"></i>
+                        </button>
+                        <button class="btn btn-link p-0 me-1" onclick="abrirVentanaEmergenteEliminacion('${machine.vm_name}')" ${machine.vm_state !== 'Apagado' ? 'disabled' : ''}>
+                            <i class="fa fa-trash icon-large" title="Delete"></i>
+                        </button>
+                    </div>`;
+
+                const row = `
+                    <tr style="background-color: ${backgroundColor}">
+                        <td>${machine.vm_name}</td>
+                        <td>${ipDisplay}</td>
+                        <td>${machine.vm_so_distro}</td>
+                        <td>${machine.vm_state}</td>
+                        <td class="button-column">${actionButtons}</td>
+                    </tr>`;
+
+                $("#machine-table tbody").append(row);
             });
         },
         error: function (error) {
-            // Elimina todo lo de la tabla
             $("#machine-table tbody").empty();
             console.error("Error al obtener datos: " + error);
         }
     });
-
 }
 
 function changeStateMachine(vm_name, state) {
